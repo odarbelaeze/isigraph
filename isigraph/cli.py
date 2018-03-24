454446e5f814184f59c6ba063c612a89ccd7a392
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2018-03-23 23:03:55 -0500

    Get labels for articles

--- isigraph/cli.py
+++ isigraph/cli.py
@@ -1,14 +1,17 @@
 """Console script for isigraph."""
 import sys
 import click
+from isigraph.isigraph import read_files
 
 
 @click.command()
-def main(args=None):
-    """Console script for isigraph."""
-    click.echo("Replace this message by putting your code into "
-               "isigraph.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.argument('files', type=click.Path(exists=True, dir_okay=False), nargs=-1)
+def main(files: list):
+    """isigraph
+
+    Load your isi files in your neo4j database.
+    """
+    read_files(files)
     return 0
 
 
--- isigraph/isigraph.py
+++ isigraph/isigraph.py
@@ -1,1 +1,37 @@
-"""Main module."""
+"""Utility functions for graph loading.
+
+There are some graphs, then we load them.
+"""
+
+
+import wostools
+
+
+def prepend(text: str, prefix: str):
+    """Prepend a prefix to a string if the string exists.
+    """
+    if not text:
+        return text
+    return prefix + text
+
+def get_label_for(article: wostools.Article):
+    author = article._data.get('AU', [''])[0].replace(',', '')
+    year = article._data.get('PY', [''])[0]
+    journal = article._data.get('J9', [''])[0]
+    parts = [author, year, journal]
+    volume = prepend(article._data.get('VL', [''])[0], 'V')
+    if volume:
+        parts.append(volume)
+    page = prepend(article._data.get('BP', [''])[0], 'P')
+    if page:
+        parts.append(page)
+    doi = prepend(article._data.get('DI', [''])[0], 'DOI ')
+    if doi:
+        parts.append(doi)
+    return ', '.join(parts)
+
+
+def read_files(filenames: list):
+    collection = wostools.CollectionLazy(*filenames)
+    for article in collection.articles:
+        print(get_label_for(article))
